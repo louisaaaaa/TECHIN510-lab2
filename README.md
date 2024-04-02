e60e5e18cacedb5b96b985922b62e5e879182337
Louisa Shi <louisa_shi@126.com>
2024-04-02 15:41:02 -0700

    update app.py

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # TECHIN510-lab2
-Repository for MSTI course 510 lab2
+Repository for MSTI course 510 lab2 
+
+## Goal
+Create a web app that reads a dataset and displays interesting data visualizations about the dataset
+
+## How to Run
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+streamlit run app.py
+```
+
+## Lessons Learned
+- Learned how to use read data from a CSV file using Pandas
+- Learned how to use selectbox and slider components in Streamlit to filter data
+- Learned how show charts and graphs using Matplotlib and Seaborn
+
+## Questions / Uncertainties
+- How can I make the data visualizations more informative and engaging?
+- How can I improve the user interface of the web app?
+- What kinds of analytical methods can I use to further explore the data?
--- app.py
+++ app.py
@@ -1,15 +1,45 @@
 import streamlit as st
 import pandas as pd 
 import seaborn as sns
-from st_aggrid import AgGrid
 
 st.set_page_config(
     page_title="Data Visualization App", 
     page_icon="🖼", 
-    page_icon=":bar_chart:", 
     layout="centered"
 )
+st.title("Data Analysis and Visualization App")
 
-st.title("Data Visualization App")
-df = pd.read_csv('IRIS.csv')
-AgGrid(df)
+st.markdown("This is a sample data visualization app built using Streamlit and AgGrid.")
+st.markdown("The data used in this app is the famous Iris dataset, which contains information about the flowers of the Iris species.") 
+st.markdown("You can explore the data by sorting, filtering.")   
+
+iris_df = pd.read_csv('IRIS.csv')
+
+# Species filter
+species_filter = st.selectbox("Filter by Species", iris_df['species'].unique())
+
+# Sepal Length slider
+sepal_length_min, sepal_length_max = st.slider("Filter by Sepal Length Range", float(iris_df['sepal_length'].min()), float(iris_df['sepal_length'].max()), (float(iris_df['sepal_length'].min()), float(iris_df['sepal_length'].max())))
+
+# Apply filters
+filtered_df = iris_df[(iris_df['species'] == species_filter) & (iris_df['sepal_length'] >= sepal_length_min) & (iris_df['sepal_length'] <= sepal_length_max)]
+
+# Display chart
+st.subheader("Filtered Data")
+st.write(filtered_df)
+
+# Plot
+st.header("Visualization")
+plot_type = st.selectbox("Select Plot Type", ["Histogram", "Pairplot"])
+
+if plot_type == "Histogram":
+    feature = st.selectbox("Select a feature", iris_df.columns[:-1])
+    fig = sns.histplot(filtered_df[feature], kde=True).get_figure()
+    st.pyplot(fig)
+
+elif plot_type == "Pairplot":
+    fig = sns.pairplot(filtered_df, hue="species")
+    st.pyplot(fig)
+    
+# Page 2: New Page
+st.title("New Page")
